f10ea2e38ff0ff7c4dda9fb7762d08c5e3c5f178
j92pruitt <j92pruitt@gmail.com>
2021-06-08 19:35:54 -0700

    Implemented logic for saving text logged in time blocks on save button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 var date = moment().format('dddd, MMMM Do');
 
-$('#currentDay').text(date)
+$('#currentDay').text(date);
+
+var savedEvents = JSON.parse(localStorage.getItem('plannerEvents'));
 
 for (i=0; i < 12; i++) {
+    if (savedEvents === null){
+        savedEvents = []
+    }
+
     if (i <= 3){
         var hour = i + 9
     } else {
@@ -17,7 +23,7 @@ for (i=0; i < 12; i++) {
 
     test = $(
         `
-        <div class="time-block row align-items-center">
+    <div class="time-block row align-items-center">
         <label class="hour col">
         ${hour}:00${ampm}
         </label>
@@ -25,7 +31,7 @@ for (i=0; i < 12; i++) {
         <textarea class="col future" data-time="${hour}:00${ampm}"></textarea>
         </div>
         <div class="col row align-items-center">
-        <button class="saveBtn col d-flex justify-content-center align-items-center">
+        <button class="saveBtn col d-flex justify-content-center align-items-center" data-index="${i}">
             <i class="fa fa-save"></i>
         </button>
         </div>
@@ -34,6 +40,12 @@ for (i=0; i < 12; i++) {
     )
 
     $('#schedule').append(test)
+
+    $(`button[data-index="${i}"]`).click(function() {
+        var index = $(this).attr('data-index');
+        savedEvents[index] = $(this).parent().prev().children().val();
+        localStorage.setItem('plannerEvents', JSON.stringify(savedEvents))
+    })
 }
 
 function updateClasses() {
@@ -43,6 +55,7 @@ function updateClasses() {
 
         if (timeBlock.hour() < currentTime.hour()) {
             $(this).removeClass('future')
+            $(this).removeClass('present')
             $(this).addClass('past')
         }
 
